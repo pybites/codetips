72fa2d5c9c0f527a7880f0cbc4d87eff69215883
bbelderbos <bobbelderbos@gmail.com>
2017-09-08 12:44:35 +0200

    remove filter

--- src/codetips/settings.py
+++ src/codetips/settings.py
@@ -37,10 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'rest_framework',
-    'django_filters',
-
     'api.apps.ApiConfig',
 ]
 
